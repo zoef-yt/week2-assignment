c8f0c1476a85c570d6a691003cde90abd830a170
Zoef <shaikhzoef36@gmail.com>
2022-01-20 16:53:40 +0530

    8 components done

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
 function toggleDialog(num) {
-	var element = document.querySelector("#dialog" + num);
+	const element = document.querySelector("#dialog" + num);
 	element.classList.toggle("dialog-container-opened");
+}
+
+function showSnackbar() {
+	const snackbar = document.querySelector("#_snackbar");
+	snackbar.classList.add("snackbar");
+	if (snackbar.classList.contains("snackbar")) {
+		setTimeout(hideSnackBar, 4000);
+	}
+}
+
+function hideSnackBar() {
+	const snackbar = document.querySelector("#_snackbar");
+	snackbar.classList.remove("snackbar");
+}
+
+const tabs = document.querySelectorAll(".tabbar-button");
+const tabContent = document.querySelectorAll(".tabbar-content");
+tabContent.forEach((content) => {
+	content.style.display = "none";
+	console.log(content.id);
+	if (content.id === "Mumbai") {
+		content.style.display = "block";
+	}
+});
+tabs.forEach((tab) => {
+	tab.addEventListener("click", function () {
+		tabs.forEach((tab) => {
+			tab.classList.remove("selected");
+		});
+		this.classList.add("selected");
+		selectTab(this.innerText);
+	});
+});
+function selectTab(element) {
+	tabContent.forEach((content) => {
+		content.style.display = "none";
+		console.log(content.id);
+		if (content.id === element) {
+			content.style.display = "block";
+		}
+	});
 }
